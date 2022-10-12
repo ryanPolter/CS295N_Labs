5af9b9714ef082356cf3298277931efb158bd9f9
Ryan <polterr@my.lanecc.edu>
2022-10-12 12:48:14 -0700

    Reuploads for Lab2

---
+++ RyanPolterSite/Controllers/YourChoiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RyanPolterSite.Controllers
+{
+    public class YourChoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
