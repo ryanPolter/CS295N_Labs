e983303c683852b651b6636bb7a8bee3ec766a81
Ryan <polterr@my.lanecc.edu>
2022-10-12 18:19:41 -0700

    Added Exercise 1-1 from textbook (Step 7 of 11, the Gibson Les Paul page)

---
+++ RyanPolterSite/RyanPolterSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RyanPolterSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
